c77bdc93c3ad34ab72ce70b39ad1d622872448b4
Stefan Konakchiev <wolverineto@abv.bg>
2019-04-04 19:45:09 +0300

    Commit Last

--- VaporStore/DataProcessor/Bonus.cs
+++ VaporStore/DataProcessor/Bonus.cs
@@ -1,13 +1,33 @@
 ﻿namespace VaporStore.DataProcessor
 {
 	using System;
-	using Data;
+    using System.Linq;
+    using Data;
 
 	public static class Bonus
 	{
 		public static string UpdateEmail(VaporStoreDbContext context, string username, string newEmail)
 		{
-			throw new NotImplementedException();
+            var user = context.Users.SingleOrDefault(e => e.Username == username);
+
+            if (user == null)
+            {
+                return $"User {username} not found";
+                //throw new ArgumentException($"User {username} not found");
+            }
+
+            var email = context.Users.Any(e => e.Email == newEmail);
+
+            if (email)
+            {
+                return $"Email {newEmail} is already taken";
+                //throw new ArgumentException($"Email {newEmail} is already taken");
+            }
+
+            user.Email = newEmail;
+            context.SaveChanges();
+
+            return $"Changed {username}'s email successfully";
 		}
 	}
 }
